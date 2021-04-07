227d016b4abe559e7e9871513064d350286f3ac3
Pierre Ollfisch <pierre.ollfisch@alumni.fh-aachen.de>
2021-04-07 19:23:10 +0200

    Split TemplateHTML into parts, fixed constructor detection and markup

--- @TemplateHTML/TemplateHTML.m
+++ @TemplateHTML/TemplateHTML.m
@@ -32,7 +32,7 @@ classdef TemplateHTML < handle
 %                     the command line.
 %
 %% Properties:
-%   name            - see inputs
+%   name            - name of m-file, after constructor without extension
 %   templateFolder  - see inputs
 %   outFolder       - see inputs
 %   styleFolder     - see inputs
@@ -90,88 +90,7 @@ classdef TemplateHTML < handle
             obj.str = string(dat{1});
             obj.verbose = verbose;
         end
-                
-        function parseStr(obj,dummyList)
-        % read in the template and replace designated parts with dummy text
-        % blocks. Loop through the dummy list, find the corresponding
-        % element in the template and replace it.
-            strTemplate = obj.str;     % string template
-            for di = 1:length(dummyList) % di = dummy index
-                currDummy = dummyList{di};
-                key     = currDummy.name;
-                filling = currDummy.filling;
-                refPath = currDummy.refPath;
-                dumType = char(currDummy.type);
-                dummyList{di,2} = currDummy.name; % for debugging in the workspace
-                
-                switch dumType
-                    case char('functRef')
-                        strTemplate = filSTR(obj,strTemplate,key,"");
-                        strTemplate = filSTR(obj,strTemplate,"{TOTAL_CALL}","");
-                        
-                        % get Block for adding List
-                        strBlock = getTPL(obj,currDummy.type);
-                        if(key == "{NAME_CALL}")
-                            keyPlace = 'functRef above';
-                        else
-                            key = "{NAME_CALL}";
-                            keyPlace = 'functReRef above';
-                        end
-                        
-                        strBlock = filSTR(obj,strBlock,key,filling);
-                        if(strcmp(refPath,"NA"))
-                            strBlock = filSTR(obj,strBlock,"{REF_CALL}",filling+".html");
-                        else
-                            strBlock = filSTR(obj,strBlock,"{REF_CALL}",refPath);
-                        end
-                        
-                        strTemplate = addBlock(obj,strTemplate,strBlock,keyPlace);
-                    case char("classBlock")
-                        % remove curly bracket text (PROPERTIES / METHODS)
-                        %   from html document
-                        strTemplate = filSTR(obj,strTemplate,key,"");
-                        % get template for this content
-                        strBlock    = getTPL(obj,currDummy.type);
-                        key         = "{SUB}"; % new key for new block
-                        strBlock        = filSTR(obj,strBlock,key,filling);
-                        if currDummy.name == "{METHODS}"
-                            keyPlace    = "methods above";
-                        else
-                            keyPlace    = "properties above";
-                        end
-                        strTemplate = addBlock(obj,strTemplate,strBlock,keyPlace);
-                    otherwise
-                        strTemplate = filSTR(obj,strTemplate,key,filling);
-                end
-            end
-            obj.str = strTemplate;
-        end % parseStr
 
-        function createHTML(obj)
-            finalStr = obj.str; % html document in txt form
-            outname = fullfile(obj.outFolder, obj.name + ".html"); % complete html path
-            currDate = date;
-            finalStr = filSTR(obj,finalStr,"{NAME}",obj.name);
-            finalStr = filSTR(obj,finalStr,"{DATE}",string(currDate));
-            finalStr = filSTR(obj,finalStr,"{YEAR}",string(currDate(end-3:end)));
-            finalStr = filSTR(obj,finalStr,"{HOME_HTML}",obj.homePath);
-            finalStr = filSTR(obj,finalStr,"{STYLE_FOLDER}",obj.styleFolder);
-            obj.str = finalStr;
-            
-            if(~isempty(obj.listFilKeys))
-                if obj.verbose
-                    disp("Open Key-Words - remove Blocks:");
-                    disp(obj.listFilKeys);
-                end
-                obj.str = removeBlocks(obj);
-            end
-            
-            % write HTML
-            fil = fopen(outname,'w');
-            fprintf(fil,"\n%s",obj.str);
-            fclose(fil); 
-        end % createHTML
-        
         function tplStr = getTPL(obj,type)
             file = obj.templFolder+type+".tpl";
             fil = fopen(file);
@@ -179,10 +98,7 @@ classdef TemplateHTML < handle
             fclose(fil);
             tplStr = string(dat{1});
         end  % getTPL      
-    end
-    
-    %% File Adjust Functions
-    methods
+
         function entireTxt = filSTR(obj,entireTxt,strReplaceMarker,strOverwrite)
         % this function looks for a string defined by "strRelaceMarker" and 
         % replaces it with the string defined in "strOverwrite". 
@@ -256,9 +172,7 @@ classdef TemplateHTML < handle
             end
             strT = obj.str;
         end % function removeBlocks
-    end % methods
-    
-    methods
+
         %% set Functions
         function set.outFolder(obj,fd)
             tmp = char(fd);
@@ -303,5 +217,11 @@ classdef TemplateHTML < handle
             listN = word;
         end
     end % methods
+    
+    %% methods defined in accompanying files
+    methods % external methods
+        createHTML(obj)
+        parseDummysIntoTemplate(obj,dummyList)
+    end % methods (external)
 end % classdef
 
