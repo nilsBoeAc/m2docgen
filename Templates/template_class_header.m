f3276c260340d8bf376846e50ed67c8aab02efd1
Pierre Ollfisch <pierre.ollfisch@alumni.fh-aachen.de>
2021-04-07 16:02:06 +0200

    Updated example templates for classes and functions

---
+++ Templates/template_class_header.m
@@ -0,0 +1,34 @@
+function template_class_header()
+% Here comes a short description.Do not use two %% 
+% It will read unitl the line starts with '%%'
+%% Description:
+%   Here comes a description. The Code-Word ist "Description" starting with
+%   two %%. The Block ends when the next comment-Block starts (next two %%)
+%   Each Block does contain a "Code-Word". Those will be stored in a dummy
+%   object. 
+%   
+%% Syntax:
+%   Here are the syntax
+%
+%% Input:
+%   Input Values
+%
+%% Properties:
+%   Output Values
+%
+%% References:
+%   [Ref1]
+%   [Ref2]
+%
+%% Disclaimer:
+%
+% Last editor:  Pierre Ollfisch
+% Last edit on: DD.MM.YYYY
+% Code version: X.Y.Z
+% Copyright (c) 2021
+
+%% ToDo / Changelog
+% - because the ToDo is seperated by an empty line from the rest of the
+%   header (and there is no block for it in the template), this list will
+%   not be on the final html page.
+end
